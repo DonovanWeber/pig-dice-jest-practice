a4ab02fba76017a49ef76c7904f8fd8c1aa7619a
Donovan Weber <donovanweber03@gmail.com>
2022-06-22 17:52:16 -0400

    3rd test passed
    Co-authored-by: waredaniel <waredaniel@users.noreply.github.com>
    Co-authored-by: JaredGran <JaredGran@users.noreply.github.com>

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -10,7 +10,12 @@ describe('Player', () => {
   });
   
   test('should add a random number to current score when a player rolls dice', () => {
-    let newPlayer = new Player(0,0,4);
+    let newPlayer = new Player(0,0,0);
     expect(newPlayer.RollDice()).toBeLessThanOrEqual(6);
+  });
+
+  test('should set round score to 0 if a player rolls 1', () => {
+    let newerPlayer = new Player(0,0,2);
+    expect(newerPlayer.AddRoundScore()).toEqual(0);
   });
 });
