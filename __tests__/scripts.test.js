05964686f60ffb763944ebec38ec6291a7c972be
Donovan Weber <donovanweber03@gmail.com>
2022-06-22 17:12:22 -0400

    2nd test pass
    Co-authored-by: waredaniel <waredaniel@users.noreply.github.com>
    Co-authored-by: JaredGran <JaredGran@users.noreply.github.com>

--- __tests__/scripts.test.js
+++ __tests__/scripts.test.js
@@ -1,3 +1,4 @@
+
 import { Player } from "./../src/scripts.js"
 
 describe('Player', () => {
@@ -6,5 +7,10 @@ describe('Player', () => {
     expect(player.totalScore).toEqual(0);
     expect(player.roundScore).toEqual(0);
     expect(player.currentScore).toEqual(0);
+  });
+  
+  test('should add a random number to current score when a player rolls dice', () => {
+    let newPlayer = new Player(0,0,4);
+    expect(newPlayer.RollDice()).toBeLessThanOrEqual(6);
   });
 });
